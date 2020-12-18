cac6d0c754addc0cd419ff6b092e6dd061dcdf59
두리 강 <kooda21@naver.com>
2020-12-18 15:38:49 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
+
+
+const App = ()=>{
+  const CustomerVO=[{
+    id:'1',
+    img:'https://placeimg.com/64/64/1',
+    name : '홍길동',
+    birthday : '950626',
+    gender:'남자',
+    job:'학생'
+  ,
+  },{
+    id:'2',
+    img:'https://placeimg.com/64/64/2',
+    name : '김영길',
+    birthday : '940626',
+    gender:'남자',
+    job:'회사원'
+  ,
+  },
+  {
+    id:'3',
+    img:'https://placeimg.com/64/64/3',
+    name : '홍진영',
+    birthday : '880626',
+    gender:'여자',
+    job:'학생'
+  ,
+  }]
+
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop Management System!</h2>
-    </div>
+    <>
+    {
+      CustomerVO.map(user=>( <Customer key={user.id} id={user.id} img={user.img} name={user.name} birthday={user.birthday} gender={user.gender} job={user.job}/>))
+    }
+    </>
   );
 }
 
